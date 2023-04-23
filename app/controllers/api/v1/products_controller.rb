d1ae08154f713e517e2d86a4f5dc3ddca31088c8
Patrick Bucher <patrick.bucher@mailbox.org>
2023-04-23 09:49:29 +0200

    product CRUD operations

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show update destroy]
   before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update]
 
   def index
     render json: Product.all
@@ -14,13 +16,34 @@ class Api::V1::ProductsController < ApplicationController
     if product.save
       render json: product, status: :created
     else
-      render json: { errors: product.errors }, statuss: :unprocessable_entity
+      render json: { errors: product.errors }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head 204
   end
 
   private
 
   def product_params
     params.require(:product).permit(:title, :price, :published)
+  end
+
+  def check_owner
+    head :forbidden unless @product.user_id == current_user&.id
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
